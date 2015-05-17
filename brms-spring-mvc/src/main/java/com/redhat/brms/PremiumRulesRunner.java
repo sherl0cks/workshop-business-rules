0ca5bd06cc33a1d06c94e3eac061f3c6fd7a4c10
sprabakkar@gmail.com <psingaram@gmail.com>
2015-05-16 22:04:58 -0400

    updated with action

--- brms-spring-mvc/src/main/java/com/redhat/brms/PremiumRulesRunner.java
+++ brms-spring-mvc/src/main/java/com/redhat/brms/PremiumRulesRunner.java
@@ -8,27 +8,44 @@ import org.kie.api.runtime.KieSession;
  * This is a sample class to launch a rule.
  */
 public class PremiumRulesRunner {
-
+	
+	static KieSession ksession;
+	
     public static final void main(String[] args) {
         try {
             // load up the knowledge base
-	        KieServices ks = KieServices.Factory.get();
-    	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+        	ksession = readKnowledgeBase();
         	
         	Driver driver = new Driver();
-        	//driver.setAge(age);
+        	driver.setAge(30);
         	Quote quote = new Quote();
+        	Vehicle vehicle = new Vehicle();
+        	vehicle.setMake("BMW");
         	if (null != driver ){
-        		kSession.insert(driver);
+        		ksession.insert(driver);
         	}
-        	if (null != quote ){
-        		kSession.insert(quote);
+        	if (null != vehicle ){
+        		ksession.insert(vehicle);
         	}
-            //kSession.fireAllRules();
+        	ksession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
-
+	private static KieSession readKnowledgeBase() throws Exception {
+		
+		KieServices ks = KieServices.Factory.get();
+	    KieContainer kContainer = ks.getKieClasspathContainer();
+	    KieSession kSession = kContainer.newKieSession();
+		
+		return kSession;
+	}
+/*	private static KieSession readKnowledgeBase2() throws Exception {
+		
+        KieServices ks = KieServices.Factory.get();
+	    KieContainer kContainer = ks.getKieClasspathContainer();
+    	KieSession kSession = kContainer.newKieSession("ksession-rules");
+		
+		return kSession;
+	}*/
 }
